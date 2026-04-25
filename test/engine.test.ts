69910c48a14090b41c76d3000829ee1e20015f78
wonbeanie <goobb2580@gmail.com>
2026-04-25 22:29:26 +0900

    test: LightDB 및 LightDBEngine에 대한 단위 테스트 추가
    - LightDBEngine의 핵심 로직(방 생성, 참여, 데이터 업데이트 등) 검증 테스트 추가
    - LightDB 엔트리 포인트의 메서드와 엔진 연동 검증 테스트 추가

---
+++ test/engine.test.ts
@@ -0,0 +1,183 @@
+import { Snapshot } from "../src/dto/snapshot.js";
+import { LightDBEngine } from "../src/lib/engine.js";
+import { DB_PATH } from "../src/types/database.js";
+import { HandlerType } from "../src/types/web-rtc.js";
+
+describe("engine 테스트", () => {
+  let engine : LightDBEngine;
+  
+  beforeEach(() => {
+    engine = new LightDBEngine();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  })
+
+  test("데이터베이스가 업데이트되면 엔진은 onUpdateComplete를 호출해야 한다.", () => {
+    const updateSpy = vi.spyOn(engine, "onUpdateComplete");
+
+    const mockData = { users: { id: 1, name: "test"} };
+    vi.spyOn(engine.db, "database", "get").mockReturnValue(mockData);
+    engine.db.roomChief = true;
+
+    engine.db.onUpdateComplete();
+    
+    expect(engine.database).toEqual(mockData);
+    expect(engine.roomChief).toBe(true);
+    expect(engine.updateTimestamp).not.toBe("");
+    expect(updateSpy).toHaveBeenCalled();
+  });
+
+  test("커스텀 키를 스토리지에 전달할 수 있어야 한다.", () => {
+    const dbSpy = vi.spyOn(engine.db, "onSetStorageKey")
+
+    engine.onSetStorageKey("TEST-KEY");
+    expect(dbSpy).toHaveBeenCalledWith("TEST-KEY");
+  });
+
+  test("방 생성시 WebRtc를 초기화하고 방장 상태가 되어야 한다.", async () => {
+    const mockPeerId = "test-peer-id";
+    const initSpy = vi.spyOn(engine.rtc, "init").mockResolvedValue(mockPeerId);
+    const updateSpy = vi.fn();
+    engine.onUpdateComplete = updateSpy;
+
+    await engine.createRoom();
+
+    expect(initSpy).toHaveBeenCalled();
+    expect(engine.roomChief).toBe(true);
+    expect(engine.db.roomChief).toBe(true);
+    expect(engine.roomId).toBe(mockPeerId);
+    expect(updateSpy).toHaveBeenCalled();
+  });
+
+  test("방 입장시 WebRtc를 초기화하고 룸에 연결을 요청해야한다.", async () => {
+    const mockPeerId = "test-peer-id";
+    const initSpy = vi.spyOn(engine.rtc, "init");
+    const connectSpy = vi.spyOn(engine.rtc, "connect");
+    const updateSpy = vi.fn();
+    engine.onUpdateComplete = updateSpy;
+
+    await engine.joinRoom(mockPeerId);
+
+    expect(initSpy).toHaveBeenCalled();
+    expect(connectSpy).toHaveBeenCalledWith(mockPeerId);
+    expect(engine.roomId).toBe(mockPeerId);
+    expect(updateSpy).toHaveBeenCalled();
+  });
+
+  test("데이터베이스의 변경사항 구독과 해제을 요청할수 있어야 한다.", () => {
+    const addDBListenerSpy = vi.spyOn(engine.db, "addDBListener");
+    const removeDBListenerSpy = vi.spyOn(engine.db, "removeDBListener");
+
+    const mockTable = "/users";
+    const mockHandler = vi.fn();
+
+    engine.on(mockTable, mockHandler);
+
+    expect(addDBListenerSpy).toHaveBeenCalledWith(mockTable, mockHandler);
+
+    engine.off(mockTable);
+
+    expect(removeDBListenerSpy).toHaveBeenCalledWith(mockTable);
+  });
+
+  test("데이터베이스의 Update, Remove, Clear를 요청할수 있어야 한다.", async () => {
+    const updateDBSpy = vi.spyOn(engine.db, "updateDB").mockResolvedValue({});
+    const removeTableSpy = vi.spyOn(engine.db, "removeTable").mockResolvedValue({});;
+
+    const mockTable = "/users";
+    const mockData = {
+      id : 1,
+      name : "wonbeanie",
+      age : 22
+    };
+
+    await engine.update(mockTable, mockData);
+    expect(updateDBSpy).toHaveBeenCalledWith(mockTable, mockData);
+
+    await engine.remove(mockTable);
+    expect(removeTableSpy).toHaveBeenCalledWith(mockTable);
+
+    updateDBSpy.mockClear();
+
+    await engine.clear();
+    expect(updateDBSpy).toHaveBeenCalledWith(DB_PATH.ROOT, {}, true);
+  });
+
+  test("WebRtc의 변경사항 구독과 해제을 요청할수 있어야 한다.", () => {
+    const setHandlerSpy = vi.spyOn(engine.rtc, "setHandler");
+
+    const mockType = HandlerType.MESSAGE;
+    const mockHandler = vi.fn();
+
+    engine.onPeer(mockType, mockHandler);
+
+    expect(setHandlerSpy).toHaveBeenCalledWith(mockType, mockHandler);
+
+    setHandlerSpy.mockClear();
+    engine.offPeer(mockType);
+
+    expect(setHandlerSpy).toHaveBeenCalledWith(mockType, expect.any(Function));
+  });
+
+  test("destory 호출 시 WebRtc, Database를 정리하고 변수들을 초기화해야한다.", () => {
+    const destroySpy = vi.spyOn(engine.db, "destroy");
+    const rtcDestroySpy = vi.spyOn(engine.rtc, "destroy");
+
+    engine.destroy();
+
+    expect(destroySpy).toHaveBeenCalled();
+    expect(rtcDestroySpy).toHaveBeenCalled();
+    expect(engine.roomId).toBeNull();
+    expect(engine.database).toEqual({});
+    expect(engine.roomChief).toBe(false);
+  });
+
+  test("방 생성 도중 오류 발생시 에러를 던져야 한다.", async () => {
+    const mockErrorMessage = "Error Test";
+    vi.spyOn(engine.rtc, "init").mockRejectedValue(new Error(mockErrorMessage));
+
+    await expect(engine.createRoom()).rejects.toThrow(mockErrorMessage);
+  });
+
+  test("방 입장 도중 오류 발생시 에러를 던져야 한다.", async () => {
+    const mockErrorMessage = "Error Test";
+    const mockTestId = "test-peer-id";
+    vi.spyOn(engine.rtc, "init").mockResolvedValue(mockTestId);
+    vi.spyOn(engine.rtc, "connect").mockImplementation(() => {
+      throw new Error(mockErrorMessage);
+    });
+
+    await expect(engine.joinRoom(mockTestId)).rejects.toThrow(mockErrorMessage);
+  });
+
+  test("생성자 콜백 함수들 강제 실행", () => {
+    const sendSpy = vi.spyOn(engine.rtc, "send").mockImplementation(() => {})
+    engine.db.onSend({id : "1", table : "/users", data : {}});
+    expect(sendSpy).toHaveBeenCalled();
+
+    const snapshotSpy = vi.spyOn(engine.db, "getSnapshot").mockImplementation(() => new Snapshot(new Map()))
+    engine.rtc.onGetSnapshot();
+    expect(snapshotSpy).toHaveBeenCalled();
+
+    const onValueSpy = vi.spyOn(engine.db, "onValue").mockImplementation(() => {})
+    engine.rtc.onUpdateDatabase({id : "1", table : "/users", data : {}});
+    expect(onValueSpy).toHaveBeenCalled();
+    
+    const syncSpy = vi.spyOn(engine.db, "syncDatabase").mockImplementation(() => {})
+    engine.rtc.onSyncDatabase({
+      database : new Map(),
+      updateTimestamp : 0
+    });
+    expect(syncSpy).toHaveBeenCalled();
+
+    engine.db.roomChief = true;
+    const resultTrue = engine.rtc.onGetIsRoomChief();
+    expect(resultTrue).toBe(true);
+
+    engine.db.roomChief = false;
+    const resultFalse = engine.rtc.onGetIsRoomChief();
+    expect(resultFalse).toBe(false);
+  });
+});
---
+++ test/index.test.ts
@@ -0,0 +1,125 @@
+import { LightDB } from "../src/index.js";
+import { LightDBEngine } from "../src/lib/engine.js";
+import { HandlerType } from "../src/types/web-rtc.js";
+
+vi.mock("../src/lib/engine", async (importOriginal) => {
+  const actual = await importOriginal<typeof LightDBEngine>();
+  return {
+    ...actual,
+    LightDBEngine: vi.fn().mockImplementation(function(){
+      return {
+        database : {},
+        roomChief : false,
+        roomId : "",
+        updateTimestamp : "",
+        onUpdateComplete: () => {},
+        createRoom : vi.fn().mockResolvedValue("test-peer-id"),
+        onSetStorageKey : (key: string) => {},
+      };
+    }),
+  };
+});
+
+describe("Entry 테스트", () => {
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  })
+
+  describe("모킹된 엔진으로 테스트",() => {
+    let mockEngineLightDB : LightDB;
+    let mockEngine : LightDBEngine;
+
+    beforeEach(() => {
+      vi.clearAllMocks();
+      mockEngineLightDB = new LightDB();
+      mockEngine = vi.mocked(LightDBEngine).mock.results[0]?.value;
+    });
+    
+    test("엔진의 상태가 변경되면 LightDB의 프로퍼티들도 업데이트되어야 한다.", async () => {
+      mockEngine.database = {data : 'updated'};
+      mockEngine.roomChief = true;
+      mockEngine.roomId = 'test-peer-id';
+      mockEngine.updateTimestamp = "2026-04-25 18:31:22";
+
+      mockEngine.onUpdateComplete();
+
+      expect(mockEngineLightDB.database).toEqual({ data : 'updated' });
+      expect(mockEngineLightDB.roomChief).toBe(true);
+      expect(mockEngineLightDB.roomId).toBe('test-peer-id');
+      expect(mockEngineLightDB.updateTimestamp).toBe("2026-04-25 18:31:22");
+    });
+
+    test("createRoom 호출 시 sorageKey가 있으면 엔진의 onSetStorageKey 먼저 호출해야 한다.", async () => {
+      const keySpy = vi.spyOn(mockEngine, "onSetStorageKey");
+
+      await mockEngineLightDB.createRoom("test-peer-id");
+
+      expect(keySpy).toHaveBeenCalledWith("test-peer-id");
+      expect(mockEngine.createRoom).toHaveBeenCalled();
+    });
+  });
+
+  describe("실제 엔진 테스트", () => {
+    let lightDB : LightDB;
+    let OriginEngine : typeof LightDBEngine;
+
+    beforeEach(async () => {
+      const {LightDBEngine : engine} : {LightDBEngine : typeof LightDBEngine} = await vi.importActual("../src/lib/engine");
+
+      OriginEngine = engine
+
+      vi.mocked(LightDBEngine).mockImplementation(function(...args){
+        return new OriginEngine(...args);
+      });
+      lightDB = new LightDB();
+    });
+
+    afterEach(() => {
+      vi.clearAllMocks();
+    })
+
+    test("데이터베이스 이벤트 구독 및 해제 요청이 엔진으로 위임되어야 한다.", () => {
+
+      const onSpy = vi.spyOn(OriginEngine.prototype, "on");
+      const offSpy = vi.spyOn(OriginEngine.prototype, "off");
+      const mockHandler = () => {};
+
+      lightDB.on("/users", mockHandler);
+      expect(onSpy).toHaveBeenCalledWith("/users", mockHandler);
+
+      lightDB.off("/users");
+      expect(offSpy).toHaveBeenCalledWith("/users");
+    });
+
+    test("피어 이벤트 구독 및 해제 요청이 엔진으로 위임되어야 한다.", () => {
+      const onSpy = vi.spyOn(OriginEngine.prototype, "onPeer");
+      const offSpy = vi.spyOn(OriginEngine.prototype, "offPeer");
+      const mockHandler = () => {};
+
+      lightDB.onPeer(HandlerType.MESSAGE, mockHandler);
+      expect(onSpy).toHaveBeenCalledWith(HandlerType.MESSAGE, mockHandler);
+
+      lightDB.offPeer(HandlerType.MESSAGE);
+      expect(offSpy).toHaveBeenCalledWith(HandlerType.MESSAGE);
+    });
+
+    test("destory 호출 시 엔진의 리소스를 정리해야 한다.", () => {
+      const destorySpy = vi.spyOn(OriginEngine.prototype, "destroy");
+
+      lightDB.destroy();
+      expect(destorySpy).toHaveBeenCalled();
+    });
+
+    test("데이터 업데이트 및 삭제 요청이 엔진으로 올바르게 위임되어야 한다.", async () => {
+      const updateSpy = vi.spyOn(OriginEngine.prototype, "update").mockResolvedValue({});
+      const clearSpy = vi.spyOn(OriginEngine.prototype, "clear").mockResolvedValue({});
+    
+      await lightDB.update("/users", { name: "mons" });
+      expect(updateSpy).toHaveBeenCalledWith("/users", { name: "mons" });
+
+      await lightDB.clear();
+      expect(clearSpy).toHaveBeenCalled();
+    });
+  })
+});
